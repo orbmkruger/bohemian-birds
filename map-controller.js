5e3b937ee8673d3d690927d091cd045491dc55f6
orbmkruger <orbmkruger@gmail.com>
2016-06-05 21:37:45 +0200

    version with icons

--- map-controller.js
+++ map-controller.js
@@ -1,51 +1,65 @@
 // Provide your access token on mapbox.com
 L.mapbox.accessToken = "pk.eyJ1Ijoib3JibWtydWdlciIsImEiOiJjaW94ZWc2bXowMGJzdmttMjF3aXlneGhtIn0.scl7vK6Iddx5vX27IB9U1A";
 // Create a map in the div #map
-var map = L.mapbox.map("map", "mapbox.streets",{
+var map = L.mapbox.map("map", "orbmkruger.0aej3kk1",{
 		minZoom: 2,
-		maxZoom: 7,
-		maxBounds: [[90,-180],[-90,180]]
+		maxZoom: 8,
+		maxBounds: [[90,-180],[-90,180]],
+		maxBoundsViscosity: 0.5,
+		tileLayer: {
+			// This map option disables world wrapping. by default, it is false.
+			continuousWorld: false,
+			// This option disables loading tiles outside of the world bounds.
+			noWrap: true
+		}
 }).fitBounds([[65,0],[-25,60]]);
+// add coordinates map to url
+var hash = L.hash(map);
 // Create popup
 var popup = new L.Popup({ autoPan: false });
 // Send mixpanel
 // mixpanel.track("map viewed");
 
+var firstStep = "First, let us know what your bucketlist is";
+var secondStep = "Curious how you can empty them? Select a country.";
+var thirdStep = "Like the bucketlist experiences? Get a free proposal!";
+$("#info").html(firstStep);
+
 // creates the scores 0 , 0.5 , 1
 var matchScore = function(props){
 	return Math.round(
     .2 * (
-			props.wonders * document.getElementById("wonders").checked +
-      props.wildlife * document.getElementById("wildlife").checked +
-      props.jungle * document.getElementById("jungle").checked +
-      props.desert * document.getElementById("desert").checked +
-      props.islands * document.getElementById("islands").checked +
-      props.winter * document.getElementById("winter").checked +
-      props.eco * document.getElementById("eco").checked +
-      props.mountains * document.getElementById("mountains").checked +
-      props.landscape * document.getElementById("landscape").checked +
-      props.wellness * document.getElementById("wellness").checked +
-      props.golfing * document.getElementById("golfing").checked +
-      props.fishing * document.getElementById("fishing").checked +
-      props.beach * document.getElementById("beach").checked +
-      props.villa * document.getElementById("villa").checked +
-      props.zipline * document.getElementById("zipline").checked +
-      props.rafting * document.getElementById("rafting").checked +
-      props.parachute * document.getElementById("parachute").checked +
-      props.biking * document.getElementById("biking").checked +
-      props.watersport * document.getElementById("watersport").checked +
-      props.scuba * document.getElementById("scuba").checked +
-      props.speed * document.getElementById("speed").checked +
-      props.bungee * document.getElementById("bungee").checked +
-      props.history * document.getElementById("history").checked +
-      props.unesco * document.getElementById("unesco").checked +
-      props.food * document.getElementById("food").checked +
-      props.museum * document.getElementById("museum").checked +
-      props.architecture * document.getElementById("architecture").checked +
-      props.locals * document.getElementById("locals").checked +
-      props.nightlife * document.getElementById("nightlife").checked +
-      props.bbirds * document.getElementById("bbirds").checked +
-      props.festival * document.getElementById("festival").checked
+			props.wonders * $("#wonders").is(":checked") +
+      props.wildlife * $("#wildlife").is(":checked") +
+      props.jungle * $("#jungle").is(":checked") +
+      props.desert * $("#desert").is(":checked") +
+      props.islands * $("#islands").is(":checked") +
+      props.winter * $("#winter").is(":checked") +
+      props.eco * $("#eco").is(":checked") +
+      props.mountains * $("#mountains").is(":checked") +
+      props.landscape * $("#landscape").is(":checked") +
+      props.wellness * $("#wellness").is(":checked") +
+      props.golfing * $("#golfing").is(":checked") +
+      props.fishing * $("#fishing").is(":checked") +
+      props.beach * $("#beach").is(":checked") +
+      props.villa * $("#villa").is(":checked") +
+      props.zipline * $("#zipline").is(":checked") +
+      props.rafting * $("#rafting").is(":checked") +
+      props.parachute * $("#parachute").is(":checked") +
+      props.biking * $("#biking").is(":checked") +
+      props.watersport * $("#watersport").is(":checked") +
+      props.scuba * $("#scuba").is(":checked") +
+      props.speed * $("#speed").is(":checked") +
+      props.bungee * $("#bungee").is(":checked") +
+      props.history * $("#history").is(":checked") +
+      props.unesco * $("#unesco").is(":checked") +
+      props.food * $("#food").is(":checked") +
+      props.museum * $("#museum").is(":checked") +
+      props.architecture * $("#architecture").is(":checked") +
+      props.locals * $("#locals").is(":checked") +
+      props.nightlife * $("#nightlife").is(":checked") +
+      props.bbirds * $("#bbirds").is(":checked") +
+      props.festival * $("#festival").is(":checked")
 		) / (
 			$(".bucketlist:checked").length
 		)
@@ -59,7 +73,7 @@ function getBorderColor(d){
 }
 
 function getRecommendation(d){
-	return d == 1   ? "Great choice to empty<br>your bucketlist!" :
+	return d == 1   ? "great choice! Click view the activities." :
 	       d == 0.5 ? "Poor match, but happy to help!" :
 				 "Sorry mate, no opportunity to empty<br>your bucketlist here";
 }
@@ -90,20 +104,12 @@ function onEachFeature(feature, layer) {
 	});
 }
 
-var closeTooltip;
-
 function mousemove(e){
 	if (map.getZoom() <= 3) {
 		var layer = e.target;
-	  popup.setLatLng(e.latlng);
-	  popup.setContent("<div class='marker-title'>" +
-			layer.feature.properties.name +
-			" " +
-			getStars(matchScore(layer.feature.properties)) +
-			"</div>" +
-			getRecommendation(matchScore(layer.feature.properties)));
-	  if (!popup._map) popup.openOn(map);
-	  window.clearTimeout(closeTooltip);
+	  $("#info").html(layer.feature.properties.name + " a " +
+			getRecommendation(matchScore(layer.feature.properties))
+		);
 	  // highlight feature
 	  layer.setStyle({
 	    weight: 3,
@@ -122,31 +128,36 @@ function mouseout(e){
 				weight: 1,
 				opacity: matchScore(layer.feature.properties)/(4/3)+0.25
 		});
-	  closeTooltip = window.setTimeout(function() {
-	      map.closePopup();
-	  }, 100);
+		$("#info").html(secondStep);
 	}
 }
 
 function zoomToFeature(e) {
 	var layer = e.target;
 	if (matchScore(layer.feature.properties) >= 0.5 ) {
+		// mixpanel.track(layer.feature.properties.name);
 		closeTooltip = window.setTimeout(function() {
 	      map.closePopup();
 	  }, 100);
-		// mixpanel.track(layer.feature.properties.name);
+		// remove bucketlist and add country-info
+		$("#bucketlist").hide();
+		$("#map").css("left","0");
+		$("#info").css("left","0");
+		$("#info").html(thirdStep);
+		$("#country > h1").html(layer.feature.properties.name);
+		$("#country").show();
 		// zoom in to selected country
 		map.fitBounds(layer.getBounds());
 		// view only activities from selected country
 		activitiesLayer.setFilter(function (feature){
 				return feature.properties["country"] === layer.feature.id;
 			})
-			.eachLayer(function(marker){
-				marker.setIcon(L.mapbox.marker.icon({
+			.eachLayer(function(layer){
+				layer.setIcon(L.mapbox.marker.icon({
 					"marker-color":"#A52A2A",
-					"marker-symbol":"star",
-					"marker-size":"small"
+					"marker-symbol":"star"
 				}));
+		    layer.bindPopup("<b>" + layer.feature.properties.title + "</b><br><img src='activity.png'>");
 			}).addTo(map);
 		/* make colors countries transparant
 		countriesLayer.eachLayer(function(layer){
@@ -171,9 +182,11 @@ $(document).ready(function(){
 			countriesLayer.addTo(map);
 		}
 		if ($(".bucketlist:checked").length == 0 ) {
+			$("#info").html(firstStep);
 			map.removeLayer(countriesLayer);
 			// show popup "please fill bucket"
 		} else {
+			$("#info").html(secondStep);
 			countriesLayer.eachLayer(function(layer){
 	      layer.setStyle({
 					color: getBorderColor(matchScore(layer.feature.properties)),
